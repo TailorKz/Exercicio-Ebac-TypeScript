5d68124672406b136a6918c3f79886ae4316eac1
MitsukiKz <tailorkunz@gmail.com>
2024-10-17 16:23:47 -0300

    finalizacao tarefa ebac

---
+++ functions.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiplicacao(5, 8));
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(saudacao("Tailor"));
---
+++ functions.ts
@@ -0,0 +1,12 @@
+function multiplicacao (n1: number, n2: number) : number {
+
+    
+    return n1 * n2;
+}
+console.log(multiplicacao(5, 8));
+
+
+function saudacao (nome: string): string {
+    return "Olá " + nome;
+}
+console.log(saudacao("Tailor"));
